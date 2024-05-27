8c7b418533afe6ae263b02eac1da028be793d21d
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-05-27 21:36:15 +0200

    ConvertBoolToString

--- ReversedStrings.swift
+++ ReversedStrings.swift
@@ -1,7 +1,7 @@
 /*Complete the solution so that it reverses the string passed into it.
- 
- 'world'  =>  'dlrow'
- 'word'   =>  'drow'*/
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'*/
 
 func reverse(_ str: String) -> String {
     return String(str.reversed())
